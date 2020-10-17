40ef2e5dd3fcc0b269ab83050f24e55e98993bd9
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-10-17 09:01:01 -0500

    New_Class

---
+++ pruebas.py
@@ -0,0 +1,16 @@
+'''i = 1
+e = 1
+while i != 100 or e != 1:
+    print(i)
+    i += 2
+print("Programa terminado")'''
+
+def calculo(i=0):
+    print("hola mundo", i)
+    i += 1
+    return(i)
+  
+i = 10  
+i = calculo(i)
+
+print(i)
